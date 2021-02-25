be6e2e3a1995dfe85ed9f34e350d4675890c8362
Rafaelmkp <rafaelmobarack@gmail.com>
2021-02-25 08:55:30 -0300

    d3 calc function

---
+++ desafio-mod3/frontend/src/helpers/calculator.js
@@ -0,0 +1,11 @@
+//option 1
+export function calcInterestArray(amount, rate, term) {
+  let cashFlow = [];
+
+  for (let i = 0; i <= term; i++) {
+    let interest = amount * (rate / 100);
+    amount = amount + interest;
+    cashFlow.push({ term: i, amount: amount, interest: interest });
+  }
+  return cashFlow;
+}
