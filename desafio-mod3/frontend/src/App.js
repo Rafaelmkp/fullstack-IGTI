6b3c9fa416ba60597e4d3f9f547f61cde06bc865
Rafaelmkp <rafaelmobarack@gmail.com>
2021-02-25 08:56:47 -0300

    app styling

--- desafio-mod3/frontend/src/App.js
+++ desafio-mod3/frontend/src/App.js
@@ -4,9 +4,24 @@ import InstallmentGroup from './components/InstallmentGroup';
 
 export default function App() {
   return (
-    <div>
-      <Form />
-      <InstallmentGroup />
+    <div style={style.flex}>
+      <h1>React - Juros Compostos</h1>
+      <Form style={style.flexChild} />
+      <InstallmentGroup style={style.flexChild} />
     </div>
   );
 }
+
+const style = {
+  flex: {
+    display: 'flex',
+    justifyContent: 'center',
+    textAlign: 'center',
+    flexDirection: 'column',
+    margin: '15px',
+  },
+
+  flexChild: {
+    padding: '10px',
+  },
+};
