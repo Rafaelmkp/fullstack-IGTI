5c83f542a4913fbff5972887b595b81611b55d44
Rafaelmkp <rafaelmobarack@gmail.com>
2021-02-25 08:58:12 -0300

    d3 instgp styling

--- desafio-mod3/frontend/src/components/InstallmentGroup.js
+++ desafio-mod3/frontend/src/components/InstallmentGroup.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import Installment from './Installment';
+import { calcInterestArray as calculator } from '../helpers/calculator.js';
 
 export default function InstallmentGroup() {
   return (
     <div>
-      <Installment />
+      <Installment style={style.installmBox} />
     </div>
   );
 }
+
+const style = {
+  installmBox: {
+    padding: '10px',
+  },
+};
