af2379e99115803b3df324d08a7435d6afce43ba
Rafaelmkp <rafaelmobarack@gmail.com>
2021-03-05 07:30:58 -0300

    d3 calculator adjust

--- desafio-mod3/frontend/src/helpers/calculator.js
+++ desafio-mod3/frontend/src/helpers/calculator.js
@@ -1,13 +1,17 @@
-//option 1
 export function calcInterestArray(capital, rate, term) {
   let cashFlow = [];
 
-  const initialAmount = capital;
   let amount = capital;
   for (let i = 1; i <= term; i++) {
     let interest = amount * (rate / 100);
     amount = amount + interest;
-    cashFlow.push({ term: i, amount: amount, interest: interest });
+    let totalRate = amount / capital;
+    cashFlow.push({
+      term: i,
+      amount: amount,
+      interest: interest,
+      totalRate: totalRate,
+    });
   }
   return cashFlow;
 }
