825818135b68e407e8b65fcf205204ad50e52be8
Rafaelmkp <rafaelmobarack@gmail.com>
2021-05-20 12:01:46 -0300

    fix(accounts): ajustar accounts.

--- trab-pratico-mod4/controllers/accounts.js
+++ trab-pratico-mod4/controllers/accounts.js
@@ -1,3 +1,5 @@
+import express from 'express';
+
 export async function loadData(_req, res, next) {
   try {
     const initialData = JSON.parse(await readFile(global.FILEACC));
@@ -12,3 +14,34 @@ export async function loadData(_req, res, next) {
     next(err);
   }
 }
+
+//item 4
+export async function deposit(req, res, next) {
+  //params: valor, agencia e conta
+  //validar se conta existe
+  //atualizar balance
+}
+
+//item 5
+export async function withdraw(req, res, next) {}
+
+//item 6
+export async function checkBalance(req, res, next) {}
+
+//item 7
+export async function excludeAccount(req, res, next) {}
+
+//item 8
+export async function transferValue(req, res, next) {}
+
+//item 9
+export async function checkAvgBalance(req, res, next) {}
+
+//item 10
+export async function checkSmallBalance(req, res, next) {}
+
+//item 11
+export async function checkWealthierClients(req, res, next) {}
+
+//item 12
+export async function transferClientsToPrivate(req, res, next) {}
