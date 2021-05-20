1efe035e0ac30ae58a9d41d12adb5ddc703c7ceb
Rafaelmkp <rafaelmobarack@gmail.com>
2021-05-20 12:00:07 -0300

    fix(index): atualizar formatacao.

--- trab-pratico-mod4/index.js
+++ trab-pratico-mod4/index.js
@@ -8,22 +8,22 @@ dotenv.config();
 global.FILEACC = 'accounts-2.json';
 
 const uri =
-	'mongodb+srv://' +
-	process.env.USERDB +
-	':' +
-	process.env.PWDDB +
-	'@cluster0.ktzob.mongodb.net/accounts?retryWrites=true&w=majority';
+  'mongodb+srv://' +
+  process.env.USERDB +
+  ':' +
+  process.env.PWDDB +
+  '@cluster0.ktzob.mongodb.net/accounts?retryWrites=true&w=majority';
 
 (async () => {
-	try {
-		await mongoose.connect(uri, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-		});
-		console.log('App conectado ao MONGODB');
-	} catch (err) {
-		console.log('App - erro ao conectar');
-	}
+  try {
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('App conectado ao MONGODB');
+  } catch (err) {
+    console.log('App - erro ao conectar');
+  }
 })();
 
 const app = express();
