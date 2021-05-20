89a841cef9d8482defd668c68ff80aa95174bde1
Rafaelmkp <rafaelmobarack@gmail.com>
2021-05-20 12:02:32 -0300

    fix(accountsRouter): ajustar declaracoes router.

--- trab-pratico-mod4/routes/accountsRouter.js
+++ trab-pratico-mod4/routes/accountsRouter.js
@@ -8,11 +8,11 @@ dotenv.config();
 
 const { readFile } = fs;
 
-const accRouter = express();
+const accRouter = express.Router();
 
 //INITIAL DATA CHARGE
 //Should only be called once, to fill the db
-accRouter.patch('/carregar-dados', accController.loadData());
+accRouter.patch('/carregar-dados', accController.loadData);
 
 accRouter.use((err, _req, _res, _next) => {
   console.log(err.stack);
